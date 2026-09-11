51aa76a318a0dc9094b476171dd0ba574634b2b2
Aman sharma <aman@Mac.lan>
2026-09-11 22:04:30 +0530

    Add Swagger OpenAPI documentation

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./openapi.yaml');
+
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const app = express();
 
 const userRoutes = require('./routes/user.routes');
 const authRoutes = require('./routes/auth.routes');
+
 const errorMiddleware = require('./middleware/error.middleware');
 
 app.use(helmet());
 app.use(express.json());
+
+app.use(
+    '/api-docs', 
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+);
+
 app.use(cookieParser());
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
